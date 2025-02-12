df9477866a0fa6922c2e91839fd2035c6aba3fb5
Rafael Gomes <rafagms505@gmail.com>
2025-02-12 16:08:03 -0300

    Banco de dados funcionando

--- apps/banco_dados/admin.py
+++ apps/banco_dados/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Usuario, Categoria, Objeto, Emprestimo, Reserva
 
-# Register your models here.
+@admin.register(Usuario)
+class AreaAdmin(admin.ModelAdmin):
+    list_display = ['nome','matricula']
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ['nome']
+
+@admin.register(Objeto)
+class ObjetoAdmin(admin.ModelAdmin):
+    list_display = ['nome','categoria']
+
+@admin.register(Emprestimo)
+class EmprestimoAdmin(admin.ModelAdmin):
+    list_display = ['usuario','objeto','dataDevolucao']
+
+@admin.register(Reserva)
+class ReservaAdmin(admin.ModelAdmin):
+    list_display = ['usuario','objeto','dataReserva']
--- apps/banco_dados/models.py
+++ apps/banco_dados/models.py
@@ -1,18 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-
 # Create your models here.
 
-'''
-
 class Usuario(AbstractUser):
-    matricula = models.IntegerField('Matricula', primary_key=True)
-    nome = models.CharField('Nome', max_length=300)   
+    matricula = models.IntegerField('Matrícula', primary_key=True)
+    nome = models.CharField('Nome', max_length=300)
     telefone = models.CharField('Telefone', max_length=12)
-    email = models.CharField('Email', max_length=200)
+    email = models.CharField('Email', max_length=200, unique = True)
 
-    USERNAME_FIELD = 'matricula'
+    REQUIRED_FIELDS = ['nome','username']
+    USERNAME_FIELD = 'email'
+
 
     def __str__(self):
         return self.nome 
@@ -27,7 +26,7 @@ class Categoria(models.Model):
 class Objeto(models.Model):
     nome = models.CharField('Nome', max_length=300)
     descricao = models.CharField('Descrição', max_length=300)
-    dataCadastro = models.DateTimeField()
+    dataCadastro = models.DateTimeField(auto_now=True)
     categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT)
 
     def __str__(self):
@@ -37,12 +36,10 @@ class Emprestimo(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.PROTECT)
     objeto = models.ForeignKey(Objeto, on_delete=models.PROTECT)
     dataEmprestimo = models.DateTimeField()
-    dataDevolucao = models.DateTimeField()
+    dataDevolucao = models.DateTimeField(null=True)
 
 class Reserva(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.PROTECT)
     objeto = models.ForeignKey(Objeto, on_delete=models.PROTECT)
     dataReserva = models.DateTimeField()
-    dataDevolucao = models.DateTimeField()
-
-'''
+    dataAtendimento = models.DateTimeField()
