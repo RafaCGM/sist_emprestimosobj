147b556b76c5e7fad07361c172c897a9163ba4d1
isac <isacszc@hotmail.com>
2025-02-12 22:28:10 -0300

    atualizei views e urls com um simples exemplo de Json

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('', view_home, name='home'),
     path('login/', view_login, name='login'),
-    path('cadastro/', view_cadastro, name='cadastro')
+    path('cadastro/', view_cadastro, name='cadastro'),
+    path('exemplo/', view_Json, name='Json')
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
 
 # EasterEgg que ninguém se importa: lá vai o condenado abrir mais uma view
@@ -12,3 +12,21 @@ def view_login(request):
 
 def view_cadastro(request):
     return render(request, 'cadastro.html')
+
+
+def view_Json(request):
+    comments = [
+        {'name' : 'Prenho Souza',
+        'username' : 'Lobo mau da bolsa',
+        'matricula' : '20231198060010'},
+        {'name' : 'Rafael',
+         'username' : 'tripa seca',
+        'matricula' : '20231198060020'
+        },
+        {'name' : 'Euller',
+         'username' : 'hair and shoulders',
+        'matricula' : '20231198060030'
+        }
+    ]
+
+    return JsonResponse({'comments' : comments})
