a1eba0ad3f83338d11fb1772b632ae354100f2a6
Rafael Gomes <rafagms505@gmail.com>
2025-02-24 16:54:14 -0300

    adicionei os códigos da api para usuarios

--- apps/core/serializers.py
+++ apps/core/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from apps.banco_dados.models import *
 
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = ['matricula','nome','username','telefone','email']
+
 class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categoria
--- apps/core/urls_api.py
+++ apps/core/urls_api.py
@@ -2,6 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    # USUARIO API URL
+    path('usuarios/listar', usuarioAPIlistar),
+    path('usuarios/adicionar', usuarioAPIadicionar),
+
+    # CATGORIA API URL
     path('categorias/listar/', categoriaAPIlistar),
     path('categorias/adicionar/', categoriaAPIadicionar),
 ]
