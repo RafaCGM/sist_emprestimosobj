720fc36cb3ace142be361ad293bdcf5e0c1ad741
Rafael Gomes <rafagms505@gmail.com>
2025-09-22 15:46:14 -0300

    banco de dados na nuvem

--- SistCOAPAC/settings.py
+++ SistCOAPAC/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'SistCOAPAC.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rafaescolar$sistemprestimosobjdb',
+        'USER': 'rafaescolar',
+        'PASSWORD': 'kakinho123',
+        'HOST': 'rafaescolar.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
 
